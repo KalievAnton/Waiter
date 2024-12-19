72e2dae282e87075bea7c72d56ad8dd1cf19ac1a
Anton Kaliev <antonfrukt@rambler.ru>
2024-12-19 12:41:02 +0300

    Initial Commit

---
+++ Waiter/WaiterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaiterApp.swift
+//  Waiter
+//
+//  Created by Тони on 19.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct WaiterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
